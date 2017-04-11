e53e197a85bdcb971658fcf2daee5fbcbc73d5a6
testkitchen.github.io <jonathan.shin@huffingtonpost.com>
2017-04-11 16:12:55 -0400

    recent
    recent

--- plasticbags/script.js
+++ plasticbags/script.js
@@ -13,15 +13,49 @@ multiplyNode(document.querySelector('.paper3'), 500, true);
 /*hide show prompt answer and input light values*/
 function myFunction() {
     var x = document.getElementById("myNumber").value;
-    if (x <= 1500) {
+    var y = x - 1000;
+    var z = x - 500;
+    document.getElementById("answer").style.display = 'block';
+  
+    if (x > 0 && x <= 500) {
       document.getElementById("prompt").style.visibility = 'hidden';
       document.getElementById("answer").style.visibility = 'visible';
       multiplyNode(document.querySelector('.bulb'), x, true);
-
+      document.getElementById("tungsten2").style.visibility = 'hidden';
+      document.getElementById("tungsten3").style.visibility = 'hidden';
+      
+      var div = document.createElement('div');
+      var list = document.getElementById('roof').lastChild;/*object text*/
+      list.appendChild(div);
+      div.id = 'popup';
+      div.innerHTML = '<h5>Keep scrolling to find answer!</h5>';
+      
+}
+    else if (x > 500 && x <= 1000) {
+      document.getElementById("prompt").style.visibility = 'hidden';
+      document.getElementById("answer").style.visibility = 'visible';
+      multiplyNode(document.querySelector('.bulb'), 500, true);
+      multiplyNode(document.querySelector('.tungsten'), z, true);
+}
+    else if (x > 1000 && x <= 1500) {
+      document.getElementById("prompt").style.visibility = 'hidden';
+      document.getElementById("answer").style.visibility = 'visible';
+      multiplyNode(document.querySelector('.bulb'), 500, true);
+      multiplyNode(document.querySelector('.tungsten'), 500, true);
+      multiplyNode(document.querySelector('.tungsten'), y, true);
 }
     else (
-      document.getElementById("demo").innerHTML = "That's BANANANANAS!"
+      document.getElementById("warning").innerHTML = "That's BANANANANAS!"
 )
+};
+
+var myNumber = 0;
+var txt = "";
+while (myNumber != 1500) {
+  myNumber = myNumber + 1;
+  var wrapper = "<div>" + myNumber + "</div>";
+  txt = txt + wrapper;
+  document.getElementById("numeros").innerHTML = txt;
 }
 
 
@@ -30,6 +64,6 @@ document.getElementById("myNumber")
     .addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
-        document.getElementById("press").click();
+        myFunction();
     }
 });
